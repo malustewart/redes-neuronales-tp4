c395b78215b1e551fd63a1e62f84dfb40aca0bb6
malustewart <mastewart@itba.edu.ar>
2025-09-30 21:46:26 -0300

    plot bar for 1.3 (todo: indicate non convergence in plot)

--- process_data.py
+++ process_data.py
@@ -20,7 +20,7 @@ def plot_overlap_histograms(overlap_data, bins=20, save_folder=".\\figs"):
             plt.title(f"Overlaps: N={N}, α={alfa}")
             plt.xlabel("Overlap m")
             plt.ylabel("Frecuencia")
-            filename = os.path.join(save_folder, f"histogram_N_{N}_alfa_{alfa}.png")
+            filename = os.path.join(save_folder, f"overlap_histogram_N_{N}_alfa_{alfa}.png")
             plt.savefig(filename)
             plt.close()
 
@@ -37,7 +37,7 @@ def plot_histograms_per_N(overlap_data, bins=20, save_folder=".\\figs"):
         plt.xlabel("Overlap m")
         plt.ylabel("Frecuencia")
         plt.legend()
-        filename = os.path.join(save_folder, f"histogram_N_{N}.png")
+        filename = os.path.join(save_folder, f"overlap_histogram_N_{N}.png")
         plt.savefig(filename)
         plt.close()
 
@@ -61,10 +61,54 @@ def plot_histograms_per_alfa(overlap_data, bins=20, save_folder=".\\figs"):
         plt.xlabel("Overlap m")
         plt.ylabel("Frecuencia")
         plt.legend()
-        filename = os.path.join(save_folder, f"histogram_alfa_{alfa}.png")
+        filename = os.path.join(save_folder, f"overlap_histogram_alfa_{alfa}.png")
         plt.savefig(filename)
         plt.close()
 
+def plot_conv_times(conv_times:dict, save_folder=".\\figs"):
+    for N in conv_times.keys():
+        for alfa in conv_times[N].keys():
+            t_s, count_s = conv_times[N][alfa]["sequential"]
+            t_p, count_p = conv_times[N][alfa]["parallel"]
+            plt.figure()
+            plt.bar(t_s, count_s, label="Secuencial", alpha=0.7, color='C0')
+            plt.bar(t_p, count_p, label="Paralelo", alpha=0.7, color='C1')
+
+            count_non_conv_s = count_s[np.where(t_s == np.inf)]
+            count_non_conv_p = count_p[np.where(t_p == np.inf)]
+            # plt.bar([-1], count_non_conv_s, alpha=0.7,color="C0")
+            # plt.bar([-1], count_non_conv_p, alpha=0.7,color="C1", hatch="///")
+
+
+
+            # # --- Sequential ---
+            # for t, c in zip(t_s, count_s):
+            #     if np.isinf(t):
+            #         # Special bar for non-convergence
+            #         plt.bar("No converge", c, label="Secuencial (no converge)", 
+            #                 alpha=0.7, hatch="///", color="C0", edgecolor="black")
+            #     else:
+            #         plt.bar(t, c, label="Secuencial", alpha=0.7, color="C0", edgecolor="black")
+
+            # # --- Parallel ---
+            # for t, c in zip(t_p, count_p):
+            #     if np.isinf(t):
+            #         plt.bar("No converge", c, label="Paralelo (no converge)", 
+            #                 alpha=0.7, hatch="\\\\\\", color="C1", edgecolor="black")
+            #     else:
+            #         plt.bar(t, c, label="Paralelo", alpha=0.7, color="C1", edgecolor="black")
+
+
+            plt.title(f"Iteraciones hasta convergencia: N={N}, α={alfa}")
+            plt.xlabel("Iteraciones hasta convergencia")
+            plt.ylabel("Frecuencia")
+            plt.legend()
+
+            # Save plot
+            filename = os.path.join(save_folder, f"conv_times_N_{N}_alfa_{alfa}.png")
+            plt.savefig(filename)
+            plt.close()
+            print(f"Saved {filename}")
 
 def load_all_overlaps(Ns, alfas):
     overlap_data = {}
@@ -78,6 +122,23 @@ def load_all_overlaps(Ns, alfas):
                 overlap_data[N][alfa] = overlaps
     return overlap_data
 
+def get_conv_times(Ns, alfas):
+    conv_data = {}
+    for N in Ns:
+        conv_data[N] = {}
+        for alfa in alfas:
+            p = int(alfa*N)
+            conv_data[N][alfa] = {}
+            filename = f".\\data\\tp4_1_3_N_{N}_p_{p}.npz"
+            with np.load(filename) as data:
+                conv_data[N][alfa]["sequential"] = np.unique(data["conv_time_sequential"], return_counts=True)
+                conv_data[N][alfa]["parallel"] = np.unique(data["conv_time_parallel"], return_counts=True)
+    return conv_data
+
+def punto_1_3_process(Ns, alfas):
+    print("******** PUNTO 1.3 PROCESAMIENTO *********")
+    conv_times = get_conv_times(Ns, alfas)
+    plot_conv_times(conv_times)
 
 def punto_1_4_process(Ns, alfas):
     print("******** PUNTO 1.4 PROCESAMIENTO *********")
@@ -90,4 +151,5 @@ def punto_1_4_process(Ns, alfas):
     plot_histograms_per_alfa(overlap_data, bins=bins)
 
 if __name__ == "__main__":
-    punto_1_4_process([500, 1000, 2000, 4000],[0.12, 0.14, 0.16, 0.18])
+    punto_1_3_process([3000],[0.1])
+    # punto_1_4_process([500, 1000, 2000, 4000],[0.12, 0.14, 0.16, 0.18])
